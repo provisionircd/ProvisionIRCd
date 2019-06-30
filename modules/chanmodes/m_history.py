cae80e345c4911dddf718e45f5a666991de02be6
Y4kuzi <support@provisionweb.org>
2019-06-30 18:37:14 +0200

    Prevents messages to being repeatedly sent

--- modules/chanmodes/m_history.py
+++ modules/chanmodes/m_history.py
@@ -22,6 +22,8 @@ def checkExpiredBacklog(localServer):
             #localServer.handle('PRIVMSG', '{} :Message expired: {}'.format(chan.name, line))
             chan.msg_backlog['lines'] = [] # Remove all lines.
             #chan.msg_backlog['lines'] = chan.msg_backlog['lines'][1:] # Remove only expired line.
+    #for user in [user for user in localServer.m_history if int(time.time()) - localServer.m_history[user]['replayed_time'] > 60]:
+    #    pass
 
 ircd.Modules.hooks.channel_destroy()
 def destroy(self, localServer, channel):
@@ -41,7 +43,20 @@ def history_msg(self, localServer, channel, msg):
             channel.msg_backlog['lines'] = channel.msg_backlog['lines'][1:]
         utc_time = float(datetime.utcnow().strftime("%s.%f"))*10
         data = (self.fullmask(), utc_time, msg)
+        if channel.msg_backlog['lines']:
+            channel.msg_backlog['previous_last'] = channel.msg_backlog['lines'][-1]
         channel.msg_backlog['lines'].append(data)
+        if channel not in localServer.m_history:
+            localServer.m_history[channel] = {}
+        for user in channel.users:
+            if user not in localServer.m_history[channel]:
+                localServer.m_history[channel][user] = {}
+                localServer.m_history[channel][user]['last'] = None
+                localServer.m_history[channel][user]['replay_time'] = int(time.time())
+            localServer.m_history[channel][user]['last'] = data
+        ### Looking for parted users.
+        for user in [user for user in list(localServer.m_history[channel]) if user not in channel.users]:
+            del localServer.m_history[channel][user]
     except Exception as ex:
         logging.exception(ex)
 
@@ -80,12 +95,31 @@ def chmode_H(self, localServer, channel, modebuf, parambuf, action, m, param):
 @ircd.Modules.hooks.local_join()
 def show_history(self, localServer, channel):
     if chmode in channel.modes and channel.msg_backlog['lines']:
-        self._send(':{} PRIVMSG {} :Displaying backlog for {}'.format(localServer.hostname, channel.name, channel.name))
-        for entry in channel.msg_backlog['lines']:
-            prefix = ''
-            timestamp = int(entry[1]/10)
-            if 'server-time' in self.caplist:
-                prefix = '@time={}.{}Z '.format(time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime(timestamp)), round(entry[1]%1000))
-            data = '{}:{} PRIVMSG {} :{}'.format(prefix, entry[0], channel.name, entry[2])
-            self._send(data)
-        self._send(':{} PRIVMSG {} :Done displaying last {} messages.'.format(localServer.hostname, channel.name, len(channel.msg_backlog['lines'])))
+        show = 0
+        if channel not in localServer.m_history:
+            localServer.m_history[channel] = {}
+        if self not in localServer.m_history[channel]:
+            localServer.m_history[channel][self] = {}
+            localServer.m_history[channel][self]['last'] = None
+            localServer.m_history[channel][self]['replay_time'] = int(time.time())
+        if 'replay_time' in localServer.m_history[channel][self] and 'last' in localServer.m_history[channel][self]:
+            if localServer.m_history[channel][self]['last'] != channel.msg_backlog['lines'][-1] or int(time.time()) - localServer.m_history[channel][self]['replay_time'] > 600:
+                ### New messages for user.
+                show = 1
+        else:
+            show = 1
+        if show:
+            self._send(':{} PRIVMSG {} :Displaying backlog for {}'.format(localServer.hostname, channel.name, channel.name))
+            for entry in channel.msg_backlog['lines']:
+                prefix = ''
+                timestamp = int(entry[1]/10)
+                if 'server-time' in self.caplist:
+                    prefix = '@time={}.{}Z '.format(time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime(timestamp)), round(entry[1]%1000))
+                data = '{}:{} PRIVMSG {} :{}'.format(prefix, entry[0], channel.name, entry[2])
+                self._send(data)
+            self._send(':{} PRIVMSG {} :Done displaying last {} messages.'.format(localServer.hostname, channel.name, len(channel.msg_backlog['lines'])))
+            localServer.m_history[channel][self]['replay_time'] = int(time.time())
+            localServer.m_history[channel][self]['last'] = channel.msg_backlog['lines'][-1]
+
+def init(self, reload):
+    self.m_history = {}
