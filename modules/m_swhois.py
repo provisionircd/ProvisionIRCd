e8ea3b6c0a401ae935f6ec7a6ad6fced54fa2afe
Y4kuzi <support@provisionweb.org>
2020-02-16 22:26:48 +0100

    2.0 beta

--- modules/m_swhois.py
+++ modules/m_swhois.py
@@ -1,24 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
 /swhois command (server)
 """
 
 import ircd
-import os
-import sys
 
-from handle.functions import _print
+@ircd.Modules.command
+class Swhois(ircd.Command):
+    def __init__(self):
+        self.params = 2
+        self.req_class = 'Server'
+        self.command = 'swhois'
 
-@ircd.Modules.params(2)
-@ircd.Modules.req_class('Server')
-@ircd.Modules.commands('swhois')
-def swhois(self, localServer, recv):
-    try:
+    def execute(self, client, recv):
         ### :source SWHOIS target :line
         ### :source SWHOIS target :
-        user = list(filter(lambda u: u.uid == recv[2] or u.nickname == recv[2], localServer.users))
+        user = list(filter(lambda u: u.uid == recv[2] or u.nickname == recv[2], self.ircd.users))
         if not user:
             return
         user = user[0]
@@ -29,10 +25,4 @@ def swhois(self, localServer, recv):
         else:
             user.swhois = []
 
-        localServer.new_sync(localServer, self, ' '.join(recv))
-
-    except Exception as ex:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        e = 'EXCEPTION: {} in file {} line {}: {}'.format(exc_type.__name__, fname, exc_tb.tb_lineno, exc_obj)
-        _print(e, server=localServer)
+        self.ircd.new_sync(self.ircd, client, ' '.join(recv))
