c2a2c1df1246c792b9351ad4f537ba825372ca4b
Y4kuzi <support@provisionweb.org>
2024-01-19 14:38:12 +0100

    3.0

--- handle/core.py
+++ handle/core.py
@@ -905,7 +905,7 @@ class Client:
         write_start = time()
         debug_out = 0
         try:
-            # test
+            # test 2
             self.local.socket.sendall(bytes(data, "utf-8"))
             self.local.bytes_sent += len(data)
             if debug_out:
