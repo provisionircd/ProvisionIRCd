b207091786087497f4ca98d74374a9298f891134
Y4kuzi <support@provisionweb.org>
2024-01-19 12:13:24 +0100

    3.0

--- handle/core.py
+++ handle/core.py
@@ -905,14 +905,17 @@ class Client:
         write_start = time() * 1000
         debug_out = 0
         try:
-            self.local.bytes_sent += self.local.socket.send(bytes(data, "utf-8"))
-            if debug_out:
-                logging.debug(f"{self.name}[{self.ip}] < {data}")
-            self.local.messages_sent += 1
-            write_done = time() * 1000
-            write_time = write_done - write_start
-            if write_time >= 100:
-                logging.warning(f"Writing to {self.name}[{self.ip}] took {write_time:.2f} milliseconds seconds. Data: {data}")
+            while data and len(data) > 0:
+                sent = self.local.socket.send(bytes(data, "utf-8"))
+                self.local.bytes_sent += sent
+                data = data[sent:]
+                if debug_out:
+                    logging.debug(f"{self.name}[{self.ip}] < {data}")
+                self.local.messages_sent += 1
+                write_done = time() * 1000
+                write_time = write_done - write_start
+                if write_time >= 100:
+                    logging.warning(f"Writing to {self.name}[{self.ip}] took {write_time:.2f} milliseconds seconds. Data: {data}")
         except Exception as ex:
             # logging.exception(ex)
             if write_time >= 1:
