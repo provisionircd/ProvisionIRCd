63391902497f6a13f5436fd677082ae2174e582b
Y4kuzi <support@provisionweb.org>
2024-09-14 23:28:14 +0200

    Minor cleanups in ircd.py

--- ircd.py
+++ ircd.py
@@ -1,8 +1,6 @@
 #!/usr/bin python3
 
-import argparse
-import sys
-import os
+import argparse, sys, os
 
 from OpenSSL.crypto import load_certificate, FILETYPE_PEM, Error
 
@@ -12,35 +10,32 @@ from handle.core import Server, IRCD
 from handle.sockets import handle_connections
 from handle.log import log
 
-if sys.version_info < (3, 10, 0):
-    print("Python version 3.10 or higher is required.")
-    sys.exit()
-
 if __name__ == "__main__":
-    if sys.platform.startswith("linux"):
-        if os.geteuid() == 0:
-            print("Do not run as root.")
-            exit()
+    if sys.platform.startswith("linux") and os.geteuid() == 0:
+        print("Do not run as root.")
+        exit()
 
     parser = argparse.ArgumentParser(description="ProvisionIRCd")
-    parser.add_argument("-c", "--conf", help="Relative path to main configuration file")
+    parser.add_argument("-c", "--conf", help="Relative path to main configuration file", default="ircd.conf")
     parser.add_argument("--debug", help="Show debug output in console", action="store_true")
     parser.add_argument("--fork", help="Fork to the background", action="store_true")
     parser.add_argument("--certfp", help="Prints the server certificate fingerprint", action="store_true")
+
     try:
-        mkp = 1
         import bcrypt
 
-        parser.add_argument("--mkpasswd", help="Generate bcrypt password.")
+        parser.add_argument("--mkpasswd", help="Generate bcrypt password")
+        mkp = 1
     except ImportError:
         mkp = 0
+
     args = parser.parse_args()
-    if not mkp:
-        args.mkpasswd = None
-    if args.mkpasswd:
-        hashed = bcrypt.hashpw(args.mkpasswd.encode("utf-8"), bcrypt.gensalt(10)).decode("utf-8")
+
+    if mkp and args.mkpasswd:
+        hashed = bcrypt.hashpw(args.mkpasswd.encode(), bcrypt.gensalt()).decode()
         print(f"Your salted password: {hashed}")
         exit()
+
     if args.certfp:
         for file in [file for file in os.listdir("tls") if file.endswith(".pem")]:
             with open("tls/" + file, "rb") as cert:
@@ -53,16 +48,13 @@ if __name__ == "__main__":
                     pass
         exit()
 
-    if not args.conf:
-        conffile = "ircd.conf"
-    else:
-        conffile = args.conf
     if sys.version_info < (3, 10, 0):
         print("Python version 3.10 or higher is required.")
         sys.exit()
+
     try:
         IRCD.me = Server()
-        if ConfigBuild(conffile=conffile, debug=args.debug).is_ok():
+        if ConfigBuild(conffile=args.conf, debug=args.debug).is_ok():
             IRCD.boot(fork=args.fork)
             handle_connections()
     except Exception as ex:
