2db2938e818160474d70ae1432d1e089e7c67cdd
Y4kuzi <support@provisionweb.org>
2025-04-08 00:10:22 +0200

    Add current account-tag to account-notify message

--- modules/ircv3/account-notify.py
+++ modules/ircv3/account-notify.py
@@ -5,9 +5,11 @@ account-notify capability
 from handle.core import IRCD, Capability, Hook
 
 
-def user_login(client):
+def user_login(client, old_account):
+    if (AccountTag := IRCD.get_attribute_from_module("AccountTag", package="modules.ircv3.account-tag")) and old_account != '*':
+        client.mtags.append(AccountTag(value=old_account))
     data = f":{client.fullmask} ACCOUNT {client.user.account}"
-    IRCD.send_to_local_common_chans(client, mtags=[], client_cap="account-notify", data=data)
+    IRCD.send_to_local_common_chans(client, mtags=client.mtags, client_cap="account-notify", data=data)
 
 
 def init(module):
